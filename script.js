131667eab8e85cbdfeaf70a3b97eb332955702ad
🇯🇲 <jermaine@jongensvantechniek.nl>
2017-11-21 13:44:18 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+const testWrapper = document.querySelector(".test-wrapper");
+const testArea = document.querySelector("#test-area");
+const originText = document.querySelector("#origin-text p").innerHTML;
+const resetButton = document.querySelector("#reset");
+const theTimer = document.querySelector(".timer");
+
