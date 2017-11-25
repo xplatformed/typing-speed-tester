d5d9e5704b451833f5d36f04aed256cdb10d2da6
🇯🇲 <jermaine@jongensvantechniek.nl>
2017-11-26 00:19:52 +0100

    Add interval functionality to trigger count timer

--- public/script.js
+++ public/script.js
@@ -4,6 +4,20 @@ const originText = document.querySelector("#origin-text p").innerHTML;
 const resetButton = document.querySelector("#reset");
 const theTimer = document.querySelector(".timer");
 
+// clock values in array
+var timer = [0,0,0,0]; // pos 0 = min, pos 1 = , pos 2 = hundredth of a second, pos 3 = thousandth of a sec
+
+// Run a standard minute/second/hundredths timer:
+function runTimer(){
+    let currentTime = timer[0] + ": " + timer[1] + ": " + timer[2];
+    theTimer.innerHTML = currentTime;
+    timer[3]++;
+
+    // Math to display different values
+    timer[0] = Math.floor((timer[3]/100)/60); //[3]/100 gives secs / 60 gives minutes timer
+    timer[1] = Math.floor((timer[3]/100) - (timer[0] * 60)); // subtract by value [0] which is minutes
+    timer[2] = Math.floor(timer[3] - (timer[1] * 100) - (timer[0] * 6000));
+}
 
 // Match the text entered with the provided text on the page
 function spellCheck() {
@@ -14,6 +28,10 @@ function spellCheck() {
 // Start the timer:
 function start() {
     let textEnteredLength = testArea.value.length;
+    // start an interval
+    if (textEnteredLength === 0) {
+        setInterval(runTimer, 10); // runTimer function every thousandth of a second using setInterval
+    }
     console.log(textEnteredLength);
 }
 
