0b265a82862dc7dc11e05aea589a44b9343db47a
🇯🇲 <jermaine@jongensvantechniek.nl>
2017-11-26 21:56:25 +0100

    Added server.js script

--- server.js
+++ server.js
@@ -1,3 +1,13 @@
-/**
- * Created by cadmin on 22/11/17.
- */
+const express = require('express');
+
+// set equal to return result form calling express s a function
+const app = express();
+const port = 3000;
+
+app.use(express.static(__dirname + '/public'));
+
+// Binds the app to a port on our machine
+app.listen(port, ()=> {
+    console.log(`Typing speed testing Server is up on port ${3000}`);
+});
+
