1521309deb84c1cb4da7c5c44c011fae9dc95cf0
🇯🇲 <jermaine@jongensvantechniek.nl>
2017-12-15 22:36:25 +0100

    lint tested

--- public/script.js
+++ public/script.js
@@ -12,62 +12,62 @@ var timerRunning = false;
 // Add leading zero to numbers 9 or below (for visual look of counter in browser):
 /** Helper function **/
 function leadingZero(time) {
-    if (time <= 9) {
-        time = "0" + time; // javascript handles "0" as a real number
-    }
-    return time;
+	if (time <= 9) {
+		time = "0" + time; // javascript handles "0" as a real number
+	}
+	return time;
 }
 
 // Run a standard minute/second/hundredths timer:
 function runTimer(){
-    let currentTime = leadingZero(timer[0]) + ": " + leadingZero(timer[1]) + ": " + leadingZero(timer[2]);
-    theTimer.innerHTML = currentTime;
-    timer[3]++;
+	let currentTime = leadingZero(timer[0]) + ": " + leadingZero(timer[1]) + ": " + leadingZero(timer[2]);
+	theTimer.innerHTML = currentTime;
+	timer[3]++;
 
-    // Math to display different values
-    timer[0] = Math.floor((timer[3]/100)/60); //[3]/100 gives secs / 60 gives minutes timer
-    timer[1] = Math.floor((timer[3]/100) - (timer[0] * 60)); // subtract by value [0] which is minutes
-    timer[2] = Math.floor(timer[3] - (timer[1] * 100) - (timer[0] * 6000));
+	// Math to display different values
+	timer[0] = Math.floor((timer[3]/100)/60); //[3]/100 gives secs / 60 gives minutes timer
+	timer[1] = Math.floor((timer[3]/100) - (timer[0] * 60)); // subtract by value [0] which is minutes
+	timer[2] = Math.floor(timer[3] - (timer[1] * 100) - (timer[0] * 6000));
 }
 
 // Match the text entered with the provided text on the page
 function spellCheck() {
-    let textEntered = testArea.value; // test area
-    let originTextMatched = originText.substring(0, textEntered.length); // string outside test area
+	let textEntered = testArea.value; // test area
+	let originTextMatched = originText.substring(0, textEntered.length); // string outside test area
 
-    if (textEntered == originText) { // test to see if string outside test area matches exactly test complete
-        clearInterval(interval); // clears the setInterval to stop the timer
-        testWrapper.style.borderColor = "#429890"; // green/ successful for short string
-    } else {
-        if (textEntered == originTextMatched) {
-            testWrapper.style.borderColor = "#65CCF3"; // blue/ matching string
-        } else {
-            testWrapper.style.borderColor = "#E95D0F"; // orange fail/error
-        }
-    }
+	if (textEntered == originText) { // test to see if string outside test area matches exactly test complete
+		clearInterval(interval); // clears the setInterval to stop the timer
+		testWrapper.style.borderColor = "#429890"; // green/ successful for short string
+	} else {
+		if (textEntered == originTextMatched) {
+			testWrapper.style.borderColor = "#65CCF3"; // blue/ matching string
+		} else {
+			testWrapper.style.borderColor = "#E95D0F"; // orange fail/error
+		}
+	}
 } // end spellCheck
 
 // Start the timer:
 function start() {
-    let textEnteredLength = testArea.value.length;
-    // start an interval
-    if (textEnteredLength === 0 && !timerRunning) {
-        timerRunning = true;
-        interval = setInterval(runTimer, 10); // runTimer function every thousandth of a second using setInterval
-    }
-    console.log(textEnteredLength);
+	let textEnteredLength = testArea.value.length;
+	// start an interval
+	if (textEnteredLength === 0 && !timerRunning) {
+		timerRunning = true;
+		interval = setInterval(runTimer, 10); // runTimer function every thousandth of a second using setInterval
+	}
+	console.log(textEnteredLength);
 }
 
 // Reset everything
 function reset() {
-   clearInterval(interval); // clear any interval process in background
-    interval = null; // prevent new interval being setup with new index number
-    timer = [0,0,0,0]; // set timer array back to zero
-    timerRunning = false; // allows us to run new interval
+	clearInterval(interval); // clear any interval process in background
+	interval = null; // prevent new interval being setup with new index number
+	timer = [0,0,0,0]; // set timer array back to zero
+	timerRunning = false; // allows us to run new interval
 
-    testArea.value = "";
-    theTimer.innerHTML = "00:00:00"; // reset timer back to zero
-    testWrapper.style.borderColor = "grey";
+	testArea.value = "";
+	theTimer.innerHTML = "00:00:00"; // reset timer back to zero
+	testWrapper.style.borderColor = "grey";
 }
 
 
